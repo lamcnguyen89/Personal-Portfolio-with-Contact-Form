92699eea11b3b6592d9d8ae7d8a543fc50a274ae
Lam Nguyen <lamcnguyen89@gmail.com>
2020-09-08 10:41:58 -0400

    Added the framework for the email route in the contact form

---
+++ routes/apiRoutes.js
@@ -0,0 +1,16 @@
+//=============================================
+// DEPENDENCIES
+// include the path module to allow the JS file to move to the correct path to the specified file
+//=============================================
+const path = require("path");
+
+//=============================================
+// ROUTING
+//=============================================
+
+module.exports = function(app) {
+    // POST- for sending form data that the user inputs in the contact form to gmail
+    app.post("/contact", function(req,res) {
+
+    })
+};
--- server.js
+++ server.js
@@ -15,12 +15,13 @@ const app = express();
 
 // Sets the initial Port that the server will listen through for client-side requests.
 // process.env.PORT is a command that means that the server will listen to whatever number is in the environmental variable PORT. 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 666
 
 // Sets up the Express app to handle data parsing using middleware.
 // json and urlencoded are both part of bodyParse in Express: https://github.com/expressjs/body-parser
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json()); 
+
 // Creates a middleware function in which to server files from a given root directory.
 app.use(express.static("public"));
 
@@ -29,6 +30,9 @@ app.use(express.static("public"));
 // BUILD ROUTES
 // Creates route files that directs the server to take certain actions when users visit or request data from various URLs 
 //=============================================
+
+// API Routes:
+require("./routes/apiRoutes")(app);
 
 // HTML Routes:
 require("./routes/htmlRoutes")(app);
