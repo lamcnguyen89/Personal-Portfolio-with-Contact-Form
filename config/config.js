0e40cb82c9096d09c022d539f1dd9f00c21513c7
Lam Nguyen <lamcnguyen89@gmail.com>
2020-09-08 11:44:59 -0400

    Added dotenv file to hide password and gmail email

---
+++ config/config.js
@@ -0,0 +1,5 @@
+
+// File to prevent my email and email password from being displayed publicly
+module.exports = {
+
+}
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,16 +1,53 @@
+// Source: https://tylerkrys.ca/blog/adding-nodemailer-email-contact-form-node-express-app
+
 //=============================================
 // DEPENDENCIES
 // include the path module to allow the JS file to move to the correct path to the specified file
 //=============================================
 const path = require("path");
+require("dotenv").config();
+
+const GMAIL_USER = process.env.GMAIL_USER
+const GMAIL_PASS = process.env.GMAIL_PASS
 
 //=============================================
 // ROUTING
 //=============================================
 
 module.exports = function(app) {
+
     // POST- for sending form data that the user inputs in the contact form to gmail
     app.post("/contact", function(req,res) {
 
-    })
+        // Create the SMTP server for Gmail
+        const smtpTrans = nodemailer.createTransport({
+            host: 'smtp.gmail.com',
+            port: 465,
+            secure: true,
+            auth: {
+                user: GMAIL_USER,
+                pass: GMAIL_PASS
+            }
+        })
+
+        // Specify what the email will look like
+        const mailOpts = {
+            from: 'Your sender info here', // This is ignored by Gmail
+            to: GMAIL_USER,
+            subject: 'New message from contact form at tylerkrys.ca',
+            text: `${req.body.name} (${req.body.email}) says: ${req.body.message}`
+        }
+
+        // Attempt to send the email
+        smtpTrans.sendMail(mailOpts, (error, response) => {
+            if (error) {
+                res.render('contact-failure') // Show a page indicating failure
+            }
+            else {
+                res.render('contact-success') // Show a page indicating success
+            }
+        });
+
+    });
+  
 };
