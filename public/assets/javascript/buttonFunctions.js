7adfe37c99ad4033fea6e064f5c0cad6db0bae50
Lam Nguyen <lamcnguyen89@gmail.com>
2021-01-09 00:21:25 -0500

    Added some buttons for practice and for better organization to hide or show content

---
+++ public/assets/javascript/buttonFunctions.js
@@ -0,0 +1,39 @@
+function showProfile() {
+  var profileRow = document.getElementById("profileRow");
+
+  if (profileRow.style.display === "none") {
+    profileRow.style.display = "block";
+  } else {
+    profileRow.style.display = "none";
+  }
+}
+
+function showSkills() {
+  var skillsRow = document.getElementById("skillsRow");
+
+  if (skillsRow.style.display === "none") {
+    skillsRow.style.display = "block";
+  } else {
+    skillsRow.style.display = "none";
+  }
+}
+
+function showPortfolio() {
+  var portfolioRow = document.getElementById("portfolioRow");
+
+  if (portfolioRow.style.display === "none") {
+    portfolioRow.style.display = "block";
+  } else {
+    portfolioRow.style.display = "none";
+  }
+}
+
+function showContact() {
+  var contactRow = document.getElementById("contactRow");
+
+  if (contactRow.style.display === "none") {
+    contactRow.style.display = "block";
+  } else {
+    contactRow.style.display = "none";
+  }
+}
